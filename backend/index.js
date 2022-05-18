3eabd39c6e00cfae88fdb3b79bc11216fcb644e7
Emerson Faria <emerson.faria.de.oliveira@ericsson.com>
2022-05-18 20:34:01 -0300

    Backend ready to receive the requests

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,36 @@
 const express = require('express')
+const bodyParser = require('body-parser');
+
 const app = express()
 const port = 3000
 
+// support parsing of application/json type post data
+app.use(bodyParser.json());
+
 app.get('/', (req, res) => {
 	res.send('Hello World!')
+})
+
+app.post('/wordCounting', (req, res) => {
+
+	console.log(req.body)
+
+	// Initializing textLen variable with an size 0
+	let textLen = 0
+
+	// Check if text is present on json received
+	if (req.body.text) {
+		let text = req.body.text
+		// Get the number of words from text received
+		textLen = text.split(" ").length
+	}
+
+	if (textLen) {
+		res.status(200).send({"Count":textLen})
+	} else {
+		res.status(400).send({ message: "Some text input is required!" })
+	}
+
 })
 
 app.listen(port, () => {
